4a790b7b226f17a87e0dbfd801c7883fb77c2773
Daniel Nolan <dnolan@aqa.org.uk>
2019-09-01 18:07:46 +0100

    add data sources

--- modules/route_table/main.tf
+++ modules/route_table/main.tf
@@ -1,1 +1,32 @@
-#
+data "azurerm_resource_group" "this" {
+  name = format("%s-%s-%s-%s-%s", var.company, var.product, var.environment, var.instance, var.region_code)
+}
+
+data "azurerm_virtual_network" "this" {
+  name                = lower(format("%s-%s-%s-%s-%s-%s", var.company, var.product, var.environment, var.instance, var.region_code, "vnt"))
+  resource_group_name = data.azurerm_resource_group.this.name
+}
+
+data "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet"
+  virtual_network_name = format("%s%s", data.azurerm_resource_group.this.name, "VNT")
+  resource_group_name  = data.azurerm_resource_group.this.name
+}
+
+data "azurerm_subnet" "web" {
+  name                 = "WebSubnet"
+  virtual_network_name = format("%s%s", data.azurerm_resource_group.this.name, "VNT")
+  resource_group_name  = data.azurerm_resource_group.this.name
+}
+
+data "azurerm_subnet" "app" {
+  name                 = "AppSubnet"
+  virtual_network_name = format("%s%s", data.azurerm_resource_group.this.name, "VNT")
+  resource_group_name  = data.azurerm_resource_group.this.name
+}
+
+data "azurerm_subnet" "data" {
+  name                 = "DataSubnet"
+  virtual_network_name = format("%s%s", data.azurerm_resource_group.this.name, "VNT")
+  resource_group_name  = data.azurerm_resource_group.this.name
+}
