1cae8c28933ac392cd95338eb26be6cbe665be7e
Daniel Nolan <dnolan@aqa.org.uk>
2019-09-01 13:57:23 +0100

    add vnet

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,4 +2,17 @@ data "azurerm_resource_group" "this" {
   name = format("%s-%s-%s-%s-%s", var.company, var.product, var.environment, var.instance, var.region_code)
 }
 
+resource "azurerm_virtual_network" "this" {
+  name                   = ToLower(format("%s-%s-%s-%s-%s-%s", var.company, var.product, var.environment, var.instance, var.region_code, "VNT"))
+  location               = data.azurerm_resource_group.this.location
+  azurerm_resource_group = data.azurerm_resource_group.this.name
+  address_space          = ["10.0.0.0/16"]
 
+  tags = {
+    product     = var.product
+    environment = var.environment
+    instance    = var.instance
+    region_code = var.region_code
+    cost_code   = "abc123"
+  }
+}
